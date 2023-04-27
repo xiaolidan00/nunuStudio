3c5215f8ab0efdaf38c701dd8a6b50b5a6dcc0ab
xiaolidan00 <764937567@qq.com>
2023-04-27 17:14:22 +0800

    fix 资源库提示类型

--- source/editor/gui/tab/asset/asset/ImageAsset.js
+++ source/editor/gui/tab/asset/asset/ImageAsset.js
@@ -1,110 +1,102 @@
-"use strict";
+'use strict';
 
-function ImageAsset(parent)
-{
-	Asset.call(this, parent);
+function ImageAsset(parent) {
+  Asset.call(this, parent);
 
-	this.preview = document.createElement("img");
-	this.preview.draggable = true;
-	this.preview.style.position = "absolute";
-	this.preview.style.top = "5%";
-	this.preview.style.left = "17%";
-	this.preview.style.width = "66%";
-	this.preview.style.height = "66%";
-	this.preview.style.objectFit = "contain";
-	this.element.appendChild(this.preview);
+  this.preview = document.createElement('img');
+  this.preview.draggable = true;
+  this.preview.style.position = 'absolute';
+  this.preview.style.top = '5%';
+  this.preview.style.left = '17%';
+  this.preview.style.width = '66%';
+  this.preview.style.height = '66%';
+  this.preview.style.objectFit = 'contain';
+  this.element.appendChild(this.preview);
 
-	this.setIcon(Global.FILE_PATH + "icons/misc/image.png");
+  this.setIcon(Global.FILE_PATH + 'icons/misc/image.png');
 
-	var self = this;
+  var self = this;
+  this.element.onmouseenter = function () {
+    self.element.title = 'Image';
+  };
 
-	//Context menu event
-	this.element.oncontextmenu = function(event)
-	{
-		var context = new ContextMenu(DocumentBody);
-		context.size.set(130, 20);
-		context.position.set(event.clientX, event.clientY);
-		
-		context.addOption(Locale.rename, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.addAction(new ChangeAction(self.asset, "name", Editor.prompt(Locale.rename + " " + Locale.image, self.asset.name)));
-			}
-		});
-		
-		context.addOption(Locale.delete, function()
-		{
-			if(self.asset !== null && Editor.confirm(Locale.delete + " " + Locale.image))
-			{
-				Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, "images"));
-			}
-		});
+  //Context menu event
+  this.element.oncontextmenu = function (event) {
+    var context = new ContextMenu(DocumentBody);
+    context.size.set(130, 20);
+    context.position.set(event.clientX, event.clientY);
 
-		context.addOption(Locale.export, function()
-		{
-			if(Nunu.runningOnDesktop())
-			{
-				FileSystem.chooseFile(function(files)
-				{
-					if(files.length > 0)
-					{
-						self.asset.export(files[0].path);
-					}
-				}, "." + self.asset.encoding, true);
-			}
-			else
-			{
-				FileSystem.chooseFileName(function(file)
-				{
-					self.asset.export(file);
-				}, "." + self.asset.encoding);
-			}
-		});
+    context.addOption(Locale.rename, function () {
+      if (self.asset !== null) {
+        Editor.addAction(
+          new ChangeAction(
+            self.asset,
+            'name',
+            Editor.prompt(Locale.rename + ' ' + Locale.image, self.asset.name)
+          )
+        );
+      }
+    });
 
-		context.addOption(Locale.copy, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), "text");
-			}
-		});
+    context.addOption(Locale.delete, function () {
+      if (self.asset !== null && Editor.confirm(Locale.delete + ' ' + Locale.image)) {
+        Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, 'images'));
+      }
+    });
 
-		context.addOption(Locale.cut, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), "text");
-				Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, "images"));
-			}
-		});
+    context.addOption(Locale.export, function () {
+      if (Nunu.runningOnDesktop()) {
+        FileSystem.chooseFile(
+          function (files) {
+            if (files.length > 0) {
+              self.asset.export(files[0].path);
+            }
+          },
+          '.' + self.asset.encoding,
+          true
+        );
+      } else {
+        FileSystem.chooseFileName(function (file) {
+          self.asset.export(file);
+        }, '.' + self.asset.encoding);
+      }
+    });
 
-		context.updateInterface();
-	};
+    context.addOption(Locale.copy, function () {
+      if (self.asset !== null) {
+        Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), 'text');
+      }
+    });
 
-	//Drag start
-	this.element.ondragstart = function(event)
-	{
-		//Insert into drag buffer
-		if(self.asset !== null)
-		{
-			event.dataTransfer.setData("uuid", self.asset.uuid);
-			DragBuffer.push(self.asset);
-		}
-	};
+    context.addOption(Locale.cut, function () {
+      if (self.asset !== null) {
+        Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), 'text');
+        Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, 'images'));
+      }
+    });
 
-	//Drag end (called after of ondrop)
-	this.element.ondragend = function(event)
-	{
-		DragBuffer.pop(self.asset.uuid);
-	};
+    context.updateInterface();
+  };
+
+  //Drag start
+  this.element.ondragstart = function (event) {
+    //Insert into drag buffer
+    if (self.asset !== null) {
+      event.dataTransfer.setData('uuid', self.asset.uuid);
+      DragBuffer.push(self.asset);
+    }
+  };
+
+  //Drag end (called after of ondrop)
+  this.element.ondragend = function (event) {
+    DragBuffer.pop(self.asset.uuid);
+  };
 }
 
 ImageAsset.prototype = Object.create(Asset.prototype);
 
-ImageAsset.prototype.attach = function(asset)
-{
-	Asset.prototype.attach.call(this, asset);
+ImageAsset.prototype.attach = function (asset) {
+  Asset.prototype.attach.call(this, asset);
 
-	this.preview.src = asset.data;
+  this.preview.src = asset.data;
 };
--- source/editor/gui/tab/asset/asset/TextureAsset.js
+++ source/editor/gui/tab/asset/asset/TextureAsset.js
@@ -1,161 +1,136 @@
-"use strict";
+'use strict';
 
-function TextureAsset(parent)
-{
-	Asset.call(this, parent);
+function TextureAsset(parent) {
+  Asset.call(this, parent);
 
-	this.setIcon(Global.FILE_PATH + "icons/misc/texture.png");
+  this.setIcon(Global.FILE_PATH + 'icons/misc/texture.png');
 
-	var self = this;
+  var self = this;
+  this.element.onmouseenter = function () {
+    self.element.title = 'Texture';
+  };
 
-	this.element.ondblclick = function()
-	{
-		var Constructor = TextureEditor;
+  this.element.ondblclick = function () {
+    var Constructor = TextureEditor;
 
-		if(self.asset instanceof VideoTexture)
-		{
-			Constructor = VideoTextureEditor;
-		}
-		else if(self.asset instanceof CanvasTexture)
-		{
-			Constructor = CanvasTextureEditor;
-		}
-		else if(self.asset instanceof CubeTexture)
-		{
-			Constructor = CubeTextureEditor;
-		}
-		else if(self.asset instanceof SpriteSheetTexture)
-		{
-			Constructor = SpriteSheetTextureEditor;
-		}
+    if (self.asset instanceof VideoTexture) {
+      Constructor = VideoTextureEditor;
+    } else if (self.asset instanceof CanvasTexture) {
+      Constructor = CanvasTextureEditor;
+    } else if (self.asset instanceof CubeTexture) {
+      Constructor = CubeTextureEditor;
+    } else if (self.asset instanceof SpriteSheetTexture) {
+      Constructor = SpriteSheetTextureEditor;
+    }
 
-		var tab = Editor.gui.tab.getTab(Constructor, self.asset);
-		
-		if(tab === null)
-		{
-			tab = Editor.gui.tab.addTab(Constructor, true);
-			tab.attach(self.asset);
-		}
+    var tab = Editor.gui.tab.getTab(Constructor, self.asset);
 
-		tab.select();
-	}
+    if (tab === null) {
+      tab = Editor.gui.tab.addTab(Constructor, true);
+      tab.attach(self.asset);
+    }
 
-	//Context menu event
-	this.element.oncontextmenu = function(event)
-	{
-		var context = new ContextMenu(DocumentBody);
-		context.size.set(130, 20);
-		context.position.set(event.clientX, event.clientY);
-		
-		context.addOption(Locale.rename, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.addAction(new ChangeAction(self.asset, "name", Editor.prompt("Rename texture", self.asset.name)));
-			}
-		});
-		
-		context.addOption(Locale.delete, function()
-		{
-			if(self.asset !== null && Editor.confirm("Delete texture?"))
-			{
-				self.asset.dispose();
-				Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, "textures"));
-			}
-		});
+    tab.select();
+  };
 
-		context.addOption(Locale.copy, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), "text");
-			}
-		});
-		
-		context.addOption(Locale.cut, function()
-		{
-			if(self.asset !== null)
-			{
-				Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), "text");
-				Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, "textures"));
-			}
-		});
+  //Context menu event
+  this.element.oncontextmenu = function (event) {
+    var context = new ContextMenu(DocumentBody);
+    context.size.set(130, 20);
+    context.position.set(event.clientX, event.clientY);
 
-		context.addOption(Locale.duplicate, function()
-		{
-			if(self.asset !== null)
-			{
-				try
-				{
-					var resources =
-					{
-						videos: {},
-						images: {},
-						fonts: {},
-						textures: {}
-					};
+    context.addOption(Locale.rename, function () {
+      if (self.asset !== null) {
+        Editor.addAction(
+          new ChangeAction(self.asset, 'name', Editor.prompt('Rename texture', self.asset.name))
+        );
+      }
+    });
 
-					//Serialize
-					var json = self.asset.toJSON(resources);
-					var images = ObjectLoader.prototype.parseImages.call(this, resources.images);
-					var videos = ObjectLoader.prototype.parseVideos.call(this, resources.videos);
+    context.addOption(Locale.delete, function () {
+      if (self.asset !== null && Editor.confirm('Delete texture?')) {
+        self.asset.dispose();
+        Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, 'textures'));
+      }
+    });
 
-					//Loader
-					var loader = new TextureLoader();
-					loader.setImages(images);
-					loader.setVideos(videos);
+    context.addOption(Locale.copy, function () {
+      if (self.asset !== null) {
+        Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), 'text');
+      }
+    });
 
-					//Load
-					var texture = loader.parse(json); 
-					texture.uuid = THREE.Math.generateUUID();
-					
-					//Add
-					Editor.program.addTexture(texture);
-					Editor.updateObjectsViewsGUI();
-				}
-				catch(e)
-				{
-					Editor.alert("Texture duplication failed\n" + e.stack);
-				}
-			}
-		});
-		context.updateInterface();
-	};
+    context.addOption(Locale.cut, function () {
+      if (self.asset !== null) {
+        Editor.clipboard.set(JSON.stringify(self.asset.toJSON()), 'text');
+        Editor.addAction(new RemoveResourceAction(self.asset, Editor.program, 'textures'));
+      }
+    });
 
-	//Drag start
-	this.element.ondragstart = function(event)
-	{
-		//Insert into drag buffer
-		if(self.asset !== null)
-		{
-			event.dataTransfer.setData("uuid", self.asset.uuid);
-			DragBuffer.push(self.asset);
-		}
-	};
+    context.addOption(Locale.duplicate, function () {
+      if (self.asset !== null) {
+        try {
+          var resources = {
+            videos: {},
+            images: {},
+            fonts: {},
+            textures: {}
+          };
 
-	//Drag end (called after of ondrop)
-	this.element.ondragend = function(event)
-	{
-		DragBuffer.pop(self.asset.uuid);
-	};
+          //Serialize
+          var json = self.asset.toJSON(resources);
+          var images = ObjectLoader.prototype.parseImages.call(this, resources.images);
+          var videos = ObjectLoader.prototype.parseVideos.call(this, resources.videos);
+
+          //Loader
+          var loader = new TextureLoader();
+          loader.setImages(images);
+          loader.setVideos(videos);
+
+          //Load
+          var texture = loader.parse(json);
+          texture.uuid = THREE.Math.generateUUID();
+
+          //Add
+          Editor.program.addTexture(texture);
+          Editor.updateObjectsViewsGUI();
+        } catch (e) {
+          Editor.alert('Texture duplication failed\n' + e.stack);
+        }
+      }
+    });
+    context.updateInterface();
+  };
+
+  //Drag start
+  this.element.ondragstart = function (event) {
+    //Insert into drag buffer
+    if (self.asset !== null) {
+      event.dataTransfer.setData('uuid', self.asset.uuid);
+      DragBuffer.push(self.asset);
+    }
+  };
+
+  //Drag end (called after of ondrop)
+  this.element.ondragend = function (event) {
+    DragBuffer.pop(self.asset.uuid);
+  };
 }
 
 TextureAsset.prototype = Object.create(Asset.prototype);
 
-TextureAsset.prototype.attach = function(asset)
-{
-	Asset.prototype.attach.call(this, asset);
+TextureAsset.prototype.attach = function (asset) {
+  Asset.prototype.attach.call(this, asset);
 
-	this.preview = TextureRenderer.generateElement(asset);
+  this.preview = TextureRenderer.generateElement(asset);
 
-	if(this.preview !== null)
-	{
-		this.preview.draggable = true;
-		this.preview.style.position = "absolute";
-		this.preview.style.top = "5%";
-		this.preview.style.left = "17%";
-		this.preview.style.width = "66%";
-		this.preview.style.height = "66%";
-		this.element.appendChild(this.preview);	
-	}
+  if (this.preview !== null) {
+    this.preview.draggable = true;
+    this.preview.style.position = 'absolute';
+    this.preview.style.top = '5%';
+    this.preview.style.left = '17%';
+    this.preview.style.width = '66%';
+    this.preview.style.height = '66%';
+    this.element.appendChild(this.preview);
+  }
 };
